1b2c2d72a4090f0d91ea826c83a9ca81a410fdd7
Anshuman Gupta <gupta.anshuman20@gmail.com>
2022-07-29 12:47:17 +0530

    Fixed datetime bugs in scheduler function

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ from flask_mail import Mail, Message
 from datetime import datetime, date, timedelta
 import os
 
+def saveBookings():
+    global bookings
+    with open("bookings.json", "w") as f:
+        dump(bookings, f)
 weekdays = {1:'monday',2:'tuesday',3:'wednesday',4:'thursday',5:'friday',6:'saturday', 7:'sunday'}
 
 app = Flask(__name__)
 mail = Mail(app)
+
+
 
 with open("teacher_availability.json") as f:
     teacher = load(f)
@@ -17,7 +23,7 @@ if os.path.exists("bookings.json"):
         bookings = load(f)
 else:
     bookings = {}
-    dump(bookings,"bookings.json")
+    saveBookings()
     
 
 
@@ -59,29 +65,38 @@ def findSchedule(date, start_time, end_time):
         Finds the next available slot and returns the booking details
         returns a dictionary with date, weekday, start_time, and end_time of the booked slot
     '''
-    if date not in bookings:
-        bookings[date] = []
-        bookings[date].append([start_time, end_time])
-        
+    dateStr = date.strftime("%d %M %Y")
+    
+    if dateStr not in bookings:
+        bookings[dateStr] = []
+        bookings[dateStr].append({
+            "start_time" : start_time.strftime("%I %p"),
+            "end_time" : end_time.strftime("%I %p")})
+        print(bookings)
+        saveBookings()
         return {"weekday":weekdays[date.isoweekday()],
-                "date":date.strftime(date, "%d %M %Y"), 
-                "start_time":date.strftime(start_time, "%I %p"), "end_time":date.strftime(end_time, "%I %p")
+                "date":date.strftime("%d %B %Y"), 
+                "start_time":start_time.strftime("%I %p"), 
+                "end_time":end_time.strftime("%I %p")
                 }
     else:
-        for record in bookings['date']:
-            if not slotsCompatible(record, [start_time,end_time]):
+        for record in bookings[dateStr]:
+            rec_start_time = datetime.strptime(record["start_time"], "%I %p")
+            rec_end_time = datetime.strptime(record["end_time"], "%I %p")
+            if not slotsCompatible([rec_start_time,rec_end_time], [start_time, end_time]):
                 # Find slot next week
                 date = date + timedelta(weeks=1)
-                return findSchedule(date)
+                return findSchedule(date, start_time, end_time)
         # To complete
     
 @app.route("/schedule", methods=['POST'])
 def schedule():
-    student_name = request.form.get("full_name")
-    email = request.form.get("email_address")
-    weekday = request.form.get("weekday")
-    start_time = request.form.get("start_time")
-    end_time = request.form.get("end_time")
+    reqParams = request.get_json()
+    student_name = reqParams.get("full_name")
+    email = reqParams.get("email_address")
+    weekday = reqParams.get("weekday").lower()
+    start_time = reqParams.get("start_time")
+    end_time = reqParams.get("end_time")
     
     # Return if no slot for that weekday
     if not checkAvailability(weekday, start_time, end_time):
@@ -97,13 +112,13 @@ def schedule():
     
     # Convert start time and endtime to datetime
     start_time = datetime.strptime(start_time, "%I %p")
-    end_time = datetime.strptime(start_time, "%I %p")
+    end_time = datetime.strptime(end_time, "%I %p")
     
     # Find suitable schedule
     booked_slot = findSchedule(currDate, start_time, end_time)
     
     # Send mail
-    sendMail(email, student_name, booked_slot['date'], booked_slot['start_time'], booked_slot['end_time'])
+    # sendMail(email, student_name, booked_slot['date'], booked_slot['start_time'], booked_slot['end_time'])
     
         
     return jsonify(
